b4689702905aaeb1356778423b183f24b2f6b2e5
ferutizi <fer.utizi@gmail.com>
2023-02-11 19:56:42 -0300

    useEffect, fetchFact, fetchCat

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cats-Facts
+
+Prueba técnica consumiendo dos apis públicas: https://catfact.ninja/fact y https://cataas.com/cat/says/hello
+
+- Facts Random: https://catfact.ninja/fact
+
+- Imagen random: https://cataas.com/cat/says/hello
+
+- Recupera un hecho aleatorio de gatos de la primera API
+
+- Recuperar la primera palabra del hecho
+
+- Muestra una imagen de un gato con la primera palabra.
+
+## Tecnologias usadas
+
+- React
+
+# Contacto
+
+- Podes mandarme un mail a: fer.utizi@gmail.com
+- Podes ver mi portfolio completo [ACA](https://ferutizi.github.io/Portfolio/)
+- Podes ver mi linkedin [ACA](https://www.linkedin.com/in/fernando-utizi-2a72a3233/)
--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import {useState, useEffect} from 'react';
+
+// - Facts Random: https://catfact.ninja/fact
+//- Imagen random: https://cataas.com/cat/says/hello
+
 function App() {
+  const [fact, setFact] = useState();
+  const [word, setWord] = useState();
+
+  useEffect(() => {
+    fetchFact();
+  }, []);
+
+  useEffect((word) => {
+    fetchCat(word);
+  }, [word]);
+
+  const fetchFact = async () => {
+    const res = await fetch('https://catfact.ninja/fact')
+    const data = await res.json();
+    setFact(data.fact);
+    setWord(data.fact.split(' ', 1)[0])
+  } 
+
+  const fetchCat = async (word) => {
+    const res = await fetch(`https://cataas.com/cat/says/${word}`)
+    console.log(res);
+    
+  }
+
+  console.log(word);
+  
+  
   return (
-    <div className="App">
-    </div>
+    <>
+      <h1>About Cats</h1>
+      {fact && 
+      <p>{fact}</p>
+    }
+    </>
   );
 }
 
